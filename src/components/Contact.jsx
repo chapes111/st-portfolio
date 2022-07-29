67e7a4f861376560cb4d83fabb89456e1dd5fa23
chapes111 <shawntasker@gmail.com>
2022-07-29 16:19:33 -0400

    Prevent form submission and url encoding

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,6 @@ import contactArrow from '../images/contact-arrow.svg'
 import blob from '../images/contact-blob.svg'
 
 export default function Contact() {
-  const [formData, setFormData] = useState({name: '', email: '', message: ''})
   const form = useRef()
 
   const sendEmail = (e) => {
@@ -16,31 +15,12 @@ export default function Contact() {
     emailjs.sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, form.current, import.meta.env.VITE_PUBLIC_KEY)
       .then((result) => {
           console.log(result.text)
-          document.getElementById('contact-form').submit()
+          document.getElementById('contact-form').reset()
+          return false
       }, (error) => {
           console.log(error.text)
+          return false
       })
-  }
-
-  const handleChange = (e) => {
-      setFormData((prevState) => ({ ...prevState, [e.target.name]: e.target.value }));
-  }
-
-  const handleSubmit = (e) => {
-      try {
-          const { name, email, message } = formData
-
-          console.log(`name: ${name}, email: ${email}, message: ${message}`)
-
-          e.preventDefault();
-
-          if (!name || !email || !message) return
-
-          alert(`name: ${name}, email: ${email}, message: ${message}`)
-      } catch (error) {
-          console.log(error)
-          throw new Error("Form submission failed")
-      }
   }
 
   return (
@@ -52,11 +32,11 @@ export default function Contact() {
         <form id='contact-form' ref={form} onSubmit={sendEmail} className="contact-form">
           <fieldset>
             <label htmlFor="name" className="contact-form--label">Name</label>
-            <input onChange={handleChange} name='name' type="text" className="contact-form--input" />
+            <input name='name' type="text" className="contact-form--input" />
             <label htmlFor="email" className="contact-form--label">Email</label>
-            <input onChange={handleChange} name='email' type="email" className="contact-form--input" />
+            <input name='email' type="email" className="contact-form--input" />
             <label htmlFor="message" className="contact-form--label">Message</label>
-            <textarea className='contact-form--textarea' name='message' onChange={handleChange} type="text" />
+            <textarea className='contact-form--textarea' name='message' type="text" />
           </fieldset>
         </form>
       </div>
